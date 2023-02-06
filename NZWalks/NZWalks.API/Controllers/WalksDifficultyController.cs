18881934ad4639a9a5eaca6ba6d23c6bd3031aee
Craig Hildebrandt <craig.hildebrandt@tearfund.org>
2023-02-06 13:17:26 +0000

    end of section 5

---
+++ NZWalks/NZWalks.API/Controllers/WalksDifficultyController.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class WalksDifficultyController: Controller
+
+    {
+        private readonly IWalkDifficultyRepository walkDifficultyRepository;
+        private readonly IMapper mapper;
+
+        public WalksDifficultyController(IWalkDifficultyRepository walkDifficultyRepository, IMapper mapper)
+        {
+            this.walkDifficultyRepository = walkDifficultyRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalkDifficultyAsync()
+        {
+            // Fetch data from database - domain walkDifficulty
+            var WalkDifficultyDomain = await walkDifficultyRepository.GetAllAsync();
+
+            // Convert domain walks to DTO Walks
+            var walkDifficultyDTO = mapper.Map<List<Models.DTO.WalkDifficulty>>(WalkDifficultyDomain);
+
+            // Return response
+            return Ok(walkDifficultyDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetWalkDifficultyAsync")]
+        public async Task<IActionResult> GetWalkDifficultyId(Guid id)
+        {
+            // Get Walk Domain object from database
+            var walkDifficulty = await walkDifficultyRepository.GetAsync(id);
+
+            if (walkDifficulty == null)
+            {
+                return NotFound();
+            }
+
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficulty);
+
+
+            // Return response
+            return Ok(walkDifficultyDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkDifficultyAsync([FromBody] Models.DTO.AddWalkDifficultyRequest addWalkDifficultyRequest)
+        {
+            // Convert DTO to Domain Object
+            var walkDifficultyDomain = new Models.Domain.WalkDifficulty
+            {
+                Code = addWalkDifficultyRequest.Code
+            };
+
+            // Pass domain object to Repository to persist this
+            walkDifficultyDomain = await walkDifficultyRepository.AddAsync(walkDifficultyDomain);
+
+            // Convert the Domain object back to DTO
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficultyDomain);
+
+            // Send DTO response back to Client
+            return CreatedAtAction(nameof(GetWalkDifficultyId), new { id = walkDifficultyDTO.Id }, walkDifficultyDTO);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Profiles/WalkDifficultyProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Profiles
+{
+    public class WalkDifficultyProfile: Profile
+    {
+
+        public WalkDifficultyProfile()
+        {
+        CreateMap<Models.Domain.WalkDifficulty, Models.DTO.WalkDifficulty>()
+            .ReverseMap();
+
+        }
+
+
+
+    }
+}
